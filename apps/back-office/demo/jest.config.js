7f95212068c52292e4b958c5b0c0af2c1b2fabcd
Misikir Adane <misikir.adane@gmail.com>
2021-12-01 12:54:20 +0300

    demo app generated

---
+++ apps/back-office/demo/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'back-office-demo',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/back-office/demo',
+};
