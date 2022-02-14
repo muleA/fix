48fd2babc0da69ce7aa4e18691a10f88e7bb683a
Misikir Adane <misikir.adane@gmail.com>
2022-02-14 13:31:48 +0300

    chore: tailwind update

--- apps/portal/eservices/pages/index.tsx
+++ apps/portal/eservices/pages/index.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
 export function Index() {
-  return <h1 className=" text-3xl  font-bold underline ">Hello world</h1>;
+  return (
+    <Container>
+      <Title>Hello world</Title>
+    </Container>
+  );
 }
 
 export default Index;
+
+const Container = styled.div.attrs({
+  className: `flex justify-center items-center h-screen container mx-auto`,
+})``;
+
+const Title = styled.h1.attrs({
+  className: `p-4 text-2xl font-bold bg-green-500 flex `,
+})`
+  font-size: 1.5em;
+  text-align: center;
+  color: #fff;
+`;
