08bc38dc6913d5a4eaf87793bee1dcc535327a89
Mulugeta Adamu <adamu.mulugeta@yahoo.com>
2022-02-23 09:59:10 +0300

    store added

---
+++ apps/back-office/service-store/store/app.api.ts
@@ -0,0 +1,8 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import axiosBaseQuery from '../shared/utility/axios-base-query';
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery(),
+  endpoints: () => ({}),
+});
