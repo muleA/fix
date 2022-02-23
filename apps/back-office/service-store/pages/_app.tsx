cf8426ca50d0c9e12407c481d9744a9b62052b82
Mulugeta Adamu <adamu.mulugeta@yahoo.com>
2022-02-23 22:19:26 +0300

    mantine component  added

--- apps/back-office/service-store/pages/_app.tsx
+++ apps/back-office/service-store/pages/_app.tsx
@@ -2,16 +2,18 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/tailwind/main.css';
 import '../styles/tabler/main.scss';
-
+import { MantineProvider } from '@mantine/core';
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title className="tw-text-red-900 bg-blue-lt">Welcome to service-store!</title>
       </Head>
+      <MantineProvider>
       <main className="app">
         <Component {...pageProps} />
       </main>
+      </MantineProvider>
     </>
   );
 }
