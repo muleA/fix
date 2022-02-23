fbb341196e0d0dc4fa8d343def38121a53ceb529
Michael <michaelwondemu195@gmail.com>
2022-02-23 15:50:04 +0300

    some config

--- apps/server/service-store/src/modules/Classification/controllers/ServiceCollections/ServiceEntry.dto.ts
+++ apps/server/service-store/src/modules/Classification/controllers/ServiceCollections/ServiceEntry.dto.ts
@@ -1,22 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsUUID,IsNumber, IsString } from 'class-validator';
-import { ServiceEntry } from '../../domain/ServiceEntrys/serviceEntry';
-   
+import { IsBoolean, IsNotEmpty, IsUUID, IsNumber, IsString } from 'class-validator';
+import { ServiceEntry } from '../../domain/serviceCollections/ServiceEntry';
+
 /**
 *A class which contains proporties of ServiceEntry that used to receive paramamer values to be updated in the database
 */
 export class UpdateServiceEntryDto {
-  
-  @ApiProperty() 
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsUUID()
   id: string;
-      
+
   @ApiProperty()
   @IsNotEmpty()
   @IsUUID()
   serviceId: string;
-      
+
   @ApiProperty()
   @IsNotEmpty()
   @IsUUID()
@@ -25,16 +25,15 @@ export class UpdateServiceEntryDto {
 *A method that mapes  UpdateServiceEntryDto object data to  ServiceEntry domain object
 *@returns ServiceEntry domain object which contains ServiceEntry  information
 */
-static fromDTO(serviceEntryDto:UpdateServiceEntryDto): ServiceEntry
-{
-const serviceEntry: ServiceEntry = new ServiceEntry();  
-serviceEntry.id=serviceEntryDto.id; 
-serviceEntry.serviceId=serviceEntryDto.serviceId; 
-serviceEntry.serviceCollectionId=serviceEntryDto.serviceCollectionId; 
+  static fromDTO(serviceEntryDto: UpdateServiceEntryDto): ServiceEntry {
+    const serviceEntry: ServiceEntry = new ServiceEntry();
+    serviceEntry.id = serviceEntryDto.id;
+    serviceEntry.serviceId = serviceEntryDto.serviceId;
+    serviceEntry.serviceCollectionId = serviceEntryDto.serviceCollectionId;
 
 
 
-return serviceEntry;
+    return serviceEntry;
   }
 }
 /**
@@ -42,30 +41,29 @@ return serviceEntry;
 *
 */
 export class CreateServiceEntryDto {
-     
-@ApiProperty() 
-@IsNotEmpty()
-@IsUUID()
-id: string;
-@ApiProperty()
-@IsNotEmpty()
-@IsUUID()
-serviceId: string;
-@ApiProperty()
-@IsNotEmpty()
-@IsUUID()
-serviceCollectionId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  serviceId: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  serviceCollectionId: string;
   /**
 *A method that mapes  CreateServiceEntryDto object data to  ServiceEntry domain object
 *@returns ServiceEntry domain object which contains ServiceEntry  information
-*/    
-static fromDTO(serviceEntryDto:CreateServiceEntryDto): ServiceEntry
-{
-const serviceEntry: ServiceEntry = new ServiceEntry();  
-serviceEntry.id=serviceEntryDto.id; 
-serviceEntry.serviceId=serviceEntryDto.serviceId; 
-serviceEntry.serviceCollectionId=serviceEntryDto.serviceCollectionId; 
- 
-     return serviceEntry;
-    }
+*/
+  static fromDTO(serviceEntryDto: CreateServiceEntryDto): ServiceEntry {
+    const serviceEntry: ServiceEntry = new ServiceEntry();
+    serviceEntry.id = serviceEntryDto.id;
+    serviceEntry.serviceId = serviceEntryDto.serviceId;
+    serviceEntry.serviceCollectionId = serviceEntryDto.serviceCollectionId;
+
+    return serviceEntry;
+  }
 }
