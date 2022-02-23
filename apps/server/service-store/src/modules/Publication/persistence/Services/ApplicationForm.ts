f1fd8e15bc5d9e324854d23ac5359153ec80b7dc
demeke <demeke.get23@gmail.com>
2022-02-23 15:47:18 +0300

    application entity removed

--- apps/server/service-store/src/modules/Publication/persistence/Services/ApplicationForm.ts
+++ apps/server/service-store/src/modules/Publication/persistence/Services/ApplicationForm.ts
@@ -3,7 +3,7 @@ import {
   Column, 
 } from "typeorm";
 
-@Entity({ name: "applicationForm" })
+//@Entity({ name: "applicationForm" })
 export class ApplicationFormEntity {
   @Column()
   title: string;
