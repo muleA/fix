1e9618e401600ab1b3f344ae8b3f549c8a713686
demeke <demeke.get23@gmail.com>
2022-02-25 15:01:25 +0300

    orm

--- apps/server/service-store/typeormconfig.ts
+++ apps/server/service-store/typeormconfig.ts
@@ -17,7 +17,7 @@ export const DbConfig = {
   database: process.env.DATABASE_NAME,
   schema: process.env.DATABASE_SCHEMA,
   autoLoadEntities: true,
-  entities: ["dist/**/**/**/**/*.entity{.ts,.js}"],
+  entities: [__dirname + "/src/**/**/**/**/*.entity{.ts,.js}"],// we reduced the searching time
 
   // We are using migrations, synchronize should be set to false.
   synchronize: true,
@@ -31,11 +31,11 @@ export const DbConfig = {
   // Allow both start:prod and start:dev to use migrations
   // __dirname is either dist or src folder, meaning either
   // the compiled js in prod or the ts in dev.
-  migrations: ["/src/migrations/**/*{.ts,.js}"],
+  migrations: ["src/migrations/**/*{.ts,.js}"],
   cli: {
     // Location of migration should be inside src folder
     // to be compiled into dist/ folder.
-    migrationsDir: "database/migrations",
+    migrationsDir: "src/migrations",
   },
 };
-//export config;
+//export config;s
