71ee4681902a5eba2a0ec44fd13f63b464c0fbc7
Habtu <>
2022-03-22 07:29:38 +0000

    Updated index.tsx

--- apps/back-office/service-store/pages/index.tsx
+++ apps/back-office/service-store/pages/index.tsx
@@ -5,7 +5,7 @@ export function Index() {
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.scss file.
-   */
+   */ 
   return (
     <div className={styles.page}>
       <div className="wrapper">
